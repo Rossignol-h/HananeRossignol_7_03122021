acaf3b660132db2e1b7b190b51a15f7e2d52170d
Rossignol <ha.rossignol@laposte.net>
2021-12-10 20:52:06 +0100

    added favicon & Home & router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue"
+import vuetify from "./plugins/vuetify"
+import App from "./App.vue"
+import VueRouter from "vue-router"
+import Routes from "./routes"
+import axios from "axios"
+
+axios.defaults.baseURL = "http://localhost:3000/api/"
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: "history",
+})
+
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+  router: router,
+}).$mount("#app")
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  methods: {
+    loginURL() {
+      this.$router.push("/login")
+    },
+    signupURL() {
+      this.$router.push("/signUp")
+    },
+  },
+}
+</script>
+<!--------------------------------------- HTML ---------------------->
+
+<!---------------------------------- Signup card --------->
+
+<template>
+  <div class="hom--container">
+    <v-row rows="2">
+      <v-col class="d-flex child-flex">
+        <v-card @click="signupURL()" class="mx-auto" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Inscription </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <!---------------------------------- Login card --------->
+      <v-col>
+        <v-card @click="loginURL()" class="mx-auto" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Connexion </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
