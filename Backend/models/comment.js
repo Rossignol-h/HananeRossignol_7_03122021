397a292f54bc28ff8c46b6429aef93637f65d335
Rossignol <ha.rossignol@laposte.net>
2021-12-10 19:25:30 +0100

    added like & comment models

---
+++ Backend/models/comment.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const { Model } = require("sequelize")
+
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    static associate(models) {
+      // define association
+      models.Comment.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false,
+        },
+        onDelete: "CASCADE",
+      }),
+        models.Comment.belongsTo(models.Post, {
+          foreignKey: {
+            allowNull: false,
+          },
+          onDelete: "CASCADE",
+        })
+    }
+  }
+  Comment.init(
+    {
+      message: { type: DataTypes.TEXT, allowNull: false },
+      username: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "Comment",
+    }
+  )
+  return Comment
+}
---
+++ Backend/models/like.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const { Model } = require("sequelize")
+
+module.exports = (sequelize, DataTypes) => {
+  class Like extends Model {
+    static associate(models) {
+      // define association
+      models.Like.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false,
+        },
+        onDelete: "CASCADE",
+      }),
+        models.Like.belongsTo(models.Post, {
+          foreignKey: {
+            allowNull: false,
+          },
+          onDelete: "CASCADE",
+        })
+    }
+  }
+  Like.init(
+    {},
+    {
+      sequelize,
+      modelName: "Like",
+    }
+  )
+  return Like
+}
